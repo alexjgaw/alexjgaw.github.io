fdef4e155d13b3a9fc44994ac35971fd158aece2
Alex Gaw <alexanderjgaw@gmail.com>
2017-05-19 14:13:31 -0500

    Continued setup.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <h1>Oh, hi!!</h1>
+  );
+};
+
+export default NavBar;
