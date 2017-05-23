9fc7267e074e38d5eb9a8348c0b839f4ef7d6574
Alex Gaw <alexanderjgaw@gmail.com>
2017-05-22 19:54:01 -0500

    Add cloudinary for image serving

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Grid } from 'react-bootstrap';
+import cloudinary from 'cloudinary';
 import NavBar from './components/NavBar';
 import Hi from './components/Hi';
 import './App.css';
 
+cloudinary.config({
+  cloud_name: 'alex-gaw'
+});
+
+console.log(cloudinary.image('sample.jpg', { alt: 'Sample' }));
+
 const App = () => {
   return (
-    <div className="App">
+    <Grid className="App" fluid>
       <NavBar />
       <Hi />
-    </div>
+    </Grid>
   );
 };
 
--- src/components/Hi/index.js
+++ src/components/Hi/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import cloudinary from 'cloudinary';
 import { Jumbotron, Row, Col } from 'react-bootstrap';
 import CircleImg from '../CircleImg';
 import alex from '../../images/alex.jpg';
 
 const Hi = () => {
+  const bgImg = cloudinary.url('cosmic-timetraveler-221383_a0hcsh',
+    {
+      width: 1500,
+      effect: ['brightness', 80]
+    });
+  const jumbotronStyle = {
+    backgroundImage: 'url(' + bgImg + ')'
+  };
+
   return (
     <div id="Hi">
-      <Jumbotron>
+      <Jumbotron style={jumbotronStyle}>
         <Row>
           <Col md={4}>
             <CircleImg path={alex} altString="Picture of Alex's smiling face." />
